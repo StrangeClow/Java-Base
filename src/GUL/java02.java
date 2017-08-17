293606c06509f464fbafa8c856b97d2300e47b00
StrangeClow <806343718@qq.com>
2017-08-17 17:38:29 +0800

    :blush:

---
+++ src/GUL/java02.java
@@ -0,0 +1,36 @@
+package GUL;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * User: zyx
+ * Date: 2017/8/17 14:43
+ * GUi面板的使用
+ * @description http://www.weixueyuan.net/view/6057.html    17行报错
+ */
+public class java02 {
+    public static MyWindowDemo mw1;
+    public static MyWindowDemo mw2;
+    public static void main(String[] args) {
+        JButton static butt1 = new JButton("我是一个按钮");
+        String name1 ="我的第一个窗口";
+        String name2 ="我的第一个窗口";
+        mw1 = new MyWindowDemo(name1,butt1,Color.blue,350,450);
+        mw1.setVisible(true);
+        JButton butt2 = new JButton("我是另一个按钮");
+        mw2 = new MyWindowDemo(name2,butt2,Color.magenta,300,400);
+        mw2.setVisible(true);
+    }
+}
+class MyWindowDemo extends JFrame {
+    public MyWindowDemo(String name,JButton button,Color c,int w,int h){
+        super();
+        setTitle(name);
+        setSize(w,h);
+        Container con = getContentPane();
+        con.add(button);
+        con.setBackground(c);
+    }
+}
